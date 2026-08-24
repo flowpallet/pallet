92f3f930a69b671a4ca3c8a131688bb948efc640
flowpallet <mabdelal3135@gmail.com>
2026-08-24 03:26:11 +0300

    fix any error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/pallet/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
